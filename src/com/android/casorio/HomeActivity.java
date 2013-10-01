3d9eb02807002531330842c9bb7f42efc88a78f9
Lucas Ferreira <lucasfe@gmail.com>
2013-10-01 18:20:24 -0300

    Adding slider menu initial
    Signed-off-by: Lucas Ferreira <lucasfe@gmail.com>

--- src/com/android/casorio/HomeActivity.java
+++ src/com/android/casorio/HomeActivity.java
@@ -2,68 +2,112 @@ package com.android.casorio;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+
 
 public class HomeActivity extends Activity {
 	
-	private DrawerLayout mDrawerLayout;
+	private class DrawerItemClickListener implements ListView.OnItemClickListener {
+	    @Override
+	    public void onItemClick(AdapterView parent, View view, int position, long id) {
+	        selectItem(position);
+	    }
+	}
+
+	/** Swaps fragments in the main content view */
+	private void selectItem(int position) {
+	    // Create a new fragment and specify the planet to show based on position
+		Toast.makeText(getApplicationContext(), "Teste", Toast.LENGTH_SHORT).show();
+	    mDrawerLayout.closeDrawer(mDrawerList);
+	}
+
+	@Override
+	public void setTitle(CharSequence title) {
+	    getActionBar().setTitle("Luqueta");
+	}
+
+	
+	private DrawerLayout mDrawerLayout;	
     private ListView mDrawerList;
     private ActionBarDrawerToggle mDrawerToggle;
+    
+    private static String[] mOptionTitles = {"Convidados","Tarefas", "Orçamento"}; 
 
 
     @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.home_activity_layout);
+		setContentView(R.layout.home_drawer_layout);
 		
 		ActionBar actionBar = getActionBar();
 		
 		
 		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
-		
-        mDrawerToggle = new ActionBarDrawerToggle(
-                this,                  /* host Activity */
-                mDrawerLayout,         /* DrawerLayout object */
-                R.drawable.ic_launcher,  /* nav drawer icon to replace 'Up' caret */
-                R.string.action_add_guest,  /* "open drawer" description */
-                R.string.action_guest_details  /* "close drawer" description */
-                ) {
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mOptionTitles));
+
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.drawable.ic_launcher, R.string.action_add_guest, R.string.action_settings) {
+
             /** Called when a drawer has settled in a completely closed state. */
             public void onDrawerClosed(View view) {
-                getActionBar().setTitle("Teste");
+                getActionBar().setTitle("Lucas");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
             }
 
             /** Called when a drawer has settled in a completely open state. */
             public void onDrawerOpened(View drawerView) {
-                getActionBar().setTitle("Teste");
+                getActionBar().setTitle("Lucas");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
             }
         };
 
-        
         // Set the drawer toggle as the DrawerListener
         mDrawerLayout.setDrawerListener(mDrawerToggle);
-
+        
         getActionBar().setDisplayHomeAsUpEnabled(true);
         getActionBar().setHomeButtonEnabled(true);
-		actionBar.show();
-
 
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-	
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.home_action_menu, menu);
-		return true;
-	}
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+          return true;
+        }
+        // Handle your other action bar items...
+
+        return super.onOptionsItemSelected(item);
+    }
 	
 	
 }
