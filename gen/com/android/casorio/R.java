69319d739bd4c9f60a68c747cde45d25cf0c2474
Lucas Ferreira <lucasfe@gmail.com>
2013-10-16 19:43:36 -0300

    Changing home layout

--- gen/com/android/casorio/R.java
+++ gen/com/android/casorio/R.java
@@ -26,20 +26,22 @@ public final class R {
     }
     public static final class drawable {
         public static final int action_about=0x7f020000;
-        public static final int ic_drawer=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int social_add_person=0x7f020003;
-        public static final int social_group=0x7f020004;
-        public static final int social_person=0x7f020005;
+        public static final int casorio_circle=0x7f020001;
+        public static final int casorio_circle_all_white=0x7f020002;
+        public static final int ic_drawer=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int social_add_person=0x7f020005;
+        public static final int social_group=0x7f020006;
+        public static final int social_person=0x7f020007;
     }
     public static final class id {
-        public static final int action_add_guest=0x7f0a002f;
-        public static final int action_call_create_task=0x7f0a0031;
-        public static final int action_call_insert=0x7f0a002d;
-        public static final int action_insert_guest=0x7f0a002e;
-        public static final int action_save=0x7f0a002c;
-        public static final int action_settings=0x7f0a0030;
-        public static final int additionalText=0x7f0a0021;
+        public static final int action_add_guest=0x7f0a0032;
+        public static final int action_call_create_task=0x7f0a0034;
+        public static final int action_call_insert=0x7f0a0030;
+        public static final int action_insert_guest=0x7f0a0031;
+        public static final int action_save=0x7f0a002f;
+        public static final int action_settings=0x7f0a0033;
+        public static final int additionalText=0x7f0a0024;
         public static final int categories_create_coast=0x7f0a0002;
         public static final int categories_create_name=0x7f0a0001;
         public static final int category_BalanceTxtView=0x7f0a0004;
@@ -48,41 +50,44 @@ public final class R {
         public static final int category_StatusTxtView=0x7f0a0006;
         public static final int category_listView=0x7f0a0007;
         public static final int container=0x7f0a0000;
-        public static final int content_frame=0x7f0a0018;
-        public static final int drawer_layout=0x7f0a0017;
-        public static final int emailText=0x7f0a0020;
+        public static final int content_frame=0x7f0a001b;
+        public static final int drawer_layout=0x7f0a001a;
+        public static final int emailText=0x7f0a0023;
         public static final int guest_details_name=0x7f0a0009;
         public static final int guest_list_count_txt=0x7f0a0008;
-        public static final int guest_radio_group_status=0x7f0a001c;
-        public static final int guest_radio_maybe=0x7f0a001e;
-        public static final int guest_radio_no=0x7f0a001f;
-        public static final int guest_radio_yes=0x7f0a001d;
+        public static final int guest_radio_group_status=0x7f0a001f;
+        public static final int guest_radio_maybe=0x7f0a0021;
+        public static final int guest_radio_no=0x7f0a0022;
+        public static final int guest_radio_yes=0x7f0a0020;
         public static final int guest_view_info_btn=0x7f0a000d;
         public static final int guest_view_name=0x7f0a000b;
         public static final int guest_view_number_of_guests_img=0x7f0a000a;
         public static final int guest_view_status=0x7f0a000c;
-        public static final int home_availableTitleTextView=0x7f0a0013;
-        public static final int home_countdown=0x7f0a000f;
-        public static final int home_spentTitleTextView=0x7f0a0012;
-        public static final int home_sumary=0x7f0a0010;
-        public static final int home_totalSpentValueTextView=0x7f0a0015;
-        public static final int home_totalTitleTextView=0x7f0a0011;
-        public static final int home_totalValueTextView=0x7f0a0014;
-        public static final int home_updates=0x7f0a0016;
-        public static final int left_drawer=0x7f0a0019;
-        public static final int nameText=0x7f0a001a;
-        public static final int taskDeleteImg=0x7f0a002a;
-        public static final int taskList=0x7f0a002b;
-        public static final int taskListCheckbox=0x7f0a0029;
-        public static final int taskNameText=0x7f0a0022;
-        public static final int task_category_spinner=0x7f0a0023;
-        public static final int task_coast_text=0x7f0a0027;
-        public static final int task_due_date_picker=0x7f0a0025;
-        public static final int task_due_date_txt=0x7f0a0024;
-        public static final int task_note_text=0x7f0a0028;
-        public static final int task_reminder_txt_view=0x7f0a0026;
+        public static final int home_availableTitleTextView=0x7f0a0016;
+        public static final int home_countdown=0x7f0a0010;
+        public static final int home_event_nametextView=0x7f0a0011;
+        public static final int home_imageProfile=0x7f0a0012;
+        public static final int home_spentTitleTextView=0x7f0a0015;
+        public static final int home_sumary=0x7f0a0013;
+        public static final int home_totalSpentValueTextView=0x7f0a0018;
+        public static final int home_totalTitleTextView=0x7f0a0014;
+        public static final int home_totalValueTextView=0x7f0a0017;
+        public static final int home_updates=0x7f0a0019;
+        public static final int imageView1=0x7f0a000f;
+        public static final int left_drawer=0x7f0a001c;
+        public static final int nameText=0x7f0a001d;
+        public static final int taskDeleteImg=0x7f0a002d;
+        public static final int taskList=0x7f0a002e;
+        public static final int taskListCheckbox=0x7f0a002c;
+        public static final int taskNameText=0x7f0a0025;
+        public static final int task_category_spinner=0x7f0a0026;
+        public static final int task_coast_text=0x7f0a002a;
+        public static final int task_due_date_picker=0x7f0a0028;
+        public static final int task_due_date_txt=0x7f0a0027;
+        public static final int task_note_text=0x7f0a002b;
+        public static final int task_reminder_txt_view=0x7f0a0029;
         public static final int txtHeader=0x7f0a000e;
-        public static final int type_spinner=0x7f0a001b;
+        public static final int type_spinner=0x7f0a001e;
     }
     public static final class layout {
         public static final int casorio_manager_layout=0x7f030000;
@@ -112,69 +117,75 @@ public final class R {
     public static final class string {
         /**  Guest related strings 
          */
-        public static final int action_add_guest=0x7f060017;
+        public static final int action_add_guest=0x7f06001d;
         /**  Task related strings 
          */
-        public static final int action_add_task=0x7f06002b;
-        public static final int action_call_create_task=0x7f06002c;
-        public static final int action_guest_details=0x7f060018;
-        public static final int action_settings=0x7f060009;
-        public static final int app_name=0x7f060007;
-        public static final int guest_canceled=0x7f06001e;
-        public static final int guest_confirmed_status=0x7f06001c;
-        public static final int guest_info_image_description=0x7f060021;
-        public static final int guest_insert_additional_hint=0x7f060029;
-        public static final int guest_insert_email_hint=0x7f060023;
-        public static final int guest_insert_guest_type_hint=0x7f060024;
-        public static final int guest_insert_name_hint=0x7f060022;
-        public static final int guest_insert_status_radio_group_title=0x7f060025;
-        public static final int guest_insert_status_radio_option_maybe=0x7f060028;
-        public static final int guest_insert_status_radio_option_no=0x7f060027;
-        public static final int guest_insert_status_radio_option_yes=0x7f060026;
-        public static final int guest_inserted_confirmation_message=0x7f06002a;
-        public static final int guest_invite_number_image_description=0x7f060020;
-        public static final int guest_list_counter=0x7f060019;
-        public static final int guest_pending_status=0x7f06001d;
-        public static final int guest_sent_status=0x7f06001f;
-        public static final int home_available_title=0x7f060005;
-        public static final int home_budget_spent=0x7f060004;
-        public static final int home_budget_title=0x7f060002;
-        public static final int home_budget_total=0x7f060003;
+        public static final int action_add_task=0x7f060031;
+        public static final int action_call_create_task=0x7f060032;
+        public static final int action_guest_details=0x7f06001e;
+        public static final int action_settings=0x7f06000b;
+        public static final int app_name=0x7f060009;
+        public static final int empyt=0x7f060001;
+        public static final int guest_canceled=0x7f060024;
+        public static final int guest_confirmed_status=0x7f060022;
+        public static final int guest_info_image_description=0x7f060027;
+        public static final int guest_insert_additional_hint=0x7f06002f;
+        public static final int guest_insert_email_hint=0x7f060029;
+        public static final int guest_insert_guest_type_hint=0x7f06002a;
+        public static final int guest_insert_name_hint=0x7f060028;
+        public static final int guest_insert_status_radio_group_title=0x7f06002b;
+        public static final int guest_insert_status_radio_option_maybe=0x7f06002e;
+        public static final int guest_insert_status_radio_option_no=0x7f06002d;
+        public static final int guest_insert_status_radio_option_yes=0x7f06002c;
+        public static final int guest_inserted_confirmation_message=0x7f060030;
+        public static final int guest_invite_number_image_description=0x7f060026;
+        public static final int guest_list_counter=0x7f06001f;
+        public static final int guest_pending_status=0x7f060023;
+        public static final int guest_sent_status=0x7f060025;
+        public static final int home_available_title=0x7f060007;
+        public static final int home_budget_spent=0x7f060006;
+        public static final int home_budget_title=0x7f060004;
+        public static final int home_budget_total=0x7f060005;
         /**  Home Strings 
          */
-        public static final int home_count_default=0x7f060001;
-        public static final int home_tasks_title=0x7f060006;
+        public static final int home_count_default=0x7f060002;
+        public static final int home_event_name_default=0x7f060003;
+        public static final int home_tasks_title=0x7f060008;
         /**  Generic Strings 
          */
         public static final int insert_menu_name=0x7f060000;
-        public static final int invalid_email=0x7f060014;
-        public static final int invalid_number=0x7f060015;
-        public static final int invalid_phone=0x7f060016;
-        public static final int key_budget=0x7f060010;
-        public static final int list_guests_header_additional_guests=0x7f06001b;
-        public static final int list_guests_header_title=0x7f06001a;
-        public static final int menu_name=0x7f060008;
+        public static final int invalid_email=0x7f06001a;
+        public static final int invalid_number=0x7f06001b;
+        public static final int invalid_phone=0x7f06001c;
+        public static final int key_budget=0x7f060012;
+        public static final int key_evt_name=0x7f060013;
+        public static final int key_image=0x7f060014;
+        public static final int list_guests_header_additional_guests=0x7f060021;
+        public static final int list_guests_header_title=0x7f060020;
+        public static final int menu_name=0x7f06000a;
         /**  Validation error messages 
          */
-        public static final int required_field=0x7f060013;
-        public static final int setting_budget_summary=0x7f060012;
-        public static final int setting_budget_title=0x7f060011;
-        public static final int settings_date_preference_title=0x7f06000f;
+        public static final int required_field=0x7f060019;
+        public static final int setting_budget_summary=0x7f060016;
+        public static final int setting_budget_title=0x7f060015;
+        public static final int setting_event_summary=0x7f060018;
+        public static final int setting_event_title=0x7f060017;
+        public static final int settings_date_preference_title=0x7f060011;
         /**  Preferences Strings 
          */
-        public static final int settings_section_title=0x7f06000e;
-        public static final int task_category_spinner_hint=0x7f06002d;
-        public static final int task_coast_title=0x7f060031;
-        public static final int task_due_date_title=0x7f06002f;
-        public static final int task_name_hint=0x7f06002e;
-        public static final int task_note_title=0x7f060030;
-        public static final int task_reminder_content_descrption=0x7f060033;
-        public static final int task_reminder_new_reminder_title=0x7f060034;
-        public static final int task_reminder_title=0x7f060032;
-        public static final int title_expenses=0x7f06000c;
-        public static final int title_guests=0x7f06000a;
-        public static final int title_remimders=0x7f06000b;
-        public static final int title_tasks=0x7f06000d;
+        public static final int settings_section_title=0x7f060010;
+        public static final int task_category_spinner_hint=0x7f060033;
+        public static final int task_coast_title=0x7f060037;
+        public static final int task_due_date_title=0x7f060035;
+        public static final int task_name_hint=0x7f060034;
+        public static final int task_note_title=0x7f060036;
+        public static final int task_reminder_content_descrption=0x7f060039;
+        public static final int task_reminder_new_reminder_title=0x7f06003a;
+        public static final int task_reminder_title=0x7f060038;
+        public static final int title_expenses=0x7f06000e;
+        public static final int title_guests=0x7f06000c;
+        public static final int title_remimders=0x7f06000d;
+        public static final int title_tasks=0x7f06000f;
     }
     public static final class style {
         /** 
--- src/com/android/casorio/home/HomeFragment.java
+++ src/com/android/casorio/home/HomeFragment.java
@@ -8,22 +8,38 @@ import org.joda.time.DateTime;
 import org.joda.time.Days;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.casorio.R;
+import com.android.casorio.settings.SettingsFragment;
+import com.android.casorio.util.FragmentCaller;
 
 public class HomeFragment extends Fragment {
 
 	TextView countdown = null;
 	TextView totalValueTxt = null;
+	TextView eventNameTxt = null;
+
+	ImageView profileImg = null;
+
+	private static int RESULT_LOAD_IMAGE = 1;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,10 +49,21 @@ public class HomeFragment extends Fragment {
 				container, false);
 
 		countdown = (TextView) rootView.findViewById(R.id.home_countdown);
+		eventNameTxt = (TextView) rootView
+				.findViewById(R.id.home_event_nametextView);
 		totalValueTxt = (TextView) rootView
 				.findViewById(R.id.home_totalValueTextView);
+		profileImg = (ImageView) rootView.findViewById(R.id.home_imageProfile);
 
 		countdown.setText(getCountdown());
+		countdown.setOnClickListener(new CallSettingsListener());
+
+		eventNameTxt.setText(getEventName());
+		eventNameTxt.setOnClickListener(new CallSettingsListener());
+
+		profileImg.setOnClickListener(new ImagePickerListener());
+		setImage(profileImg);
+
 		totalValueTxt.setText(getTotalValue());
 		return rootView;
 
@@ -56,7 +83,7 @@ public class HomeFragment extends Fragment {
 						.parse(returnValue);
 				days = Days.daysBetween(new DateTime(new Date()),
 						new DateTime(date)).getDays();
-				returnValue = "Faltam " + days + " dias para seu casamento";
+				returnValue = String.valueOf(days);
 
 			} catch (ParseException e) {
 
@@ -64,7 +91,7 @@ public class HomeFragment extends Fragment {
 			}
 
 		} else {
-			returnValue = "Please select a date";
+			returnValue = getString(R.string.home_count_default);
 		}
 
 		return returnValue;
@@ -73,9 +100,139 @@ public class HomeFragment extends Fragment {
 	private String getTotalValue() {
 		SharedPreferences preferences = PreferenceManager
 				.getDefaultSharedPreferences(this.getActivity());
-		String returnValue = preferences.getString(getString(R.string.key_budget), "");
+		String returnValue = preferences.getString(
+				getString(R.string.key_budget), "");
 
 		return returnValue;
 	}
-	
+
+	private String getEventName() {
+		SharedPreferences preferences = PreferenceManager
+				.getDefaultSharedPreferences(this.getActivity());
+		String returnValue = preferences.getString(
+				getString(R.string.key_evt_name),
+				getString(R.string.home_event_name_default));
+
+		return returnValue;
+	}
+
+	private void setImage(ImageView imageViewer) {
+
+		String imgPath = getImagePath();
+
+		if (!TextUtils.isEmpty(imgPath)) {
+			imageViewer.setImageBitmap(decodeSampledBitmapFromFile(imgPath, 90,
+					90));
+
+		}
+
+	}
+
+	private String getImagePath() {
+		SharedPreferences preferences = PreferenceManager
+				.getDefaultSharedPreferences(this.getActivity());
+		String returnValue = preferences.getString(
+				getString(R.string.key_image), "");
+
+		return returnValue;
+	}
+
+	private void setImagePath(String path) {
+		SharedPreferences preferences = PreferenceManager
+				.getDefaultSharedPreferences(this.getActivity());
+		SharedPreferences.Editor editor = preferences.edit();
+		editor.putString(getString(R.string.key_image), path);
+		editor.commit();
+	}
+
+	class CallSettingsListener implements OnClickListener {
+
+		@Override
+		public void onClick(View v) {
+			FragmentCaller.callFragment(getActivity(), new SettingsFragment());
+		}
+
+	}
+
+	class ImagePickerListener implements OnClickListener {
+
+		@Override
+		public void onClick(View v) {
+			Intent i = new Intent(
+					Intent.ACTION_PICK,
+					android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+			startActivityForResult(i, RESULT_LOAD_IMAGE);
+		}
+
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		getActivity();
+		if (requestCode == RESULT_LOAD_IMAGE
+				&& resultCode == Activity.RESULT_OK && null != data) {
+			Uri selectedImage = data.getData();
+			String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+			Cursor cursor = getActivity().getContentResolver().query(
+					selectedImage, filePathColumn, null, null, null);
+			cursor.moveToFirst();
+
+			int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+			String picturePath = cursor.getString(columnIndex);
+			cursor.close();
+
+			setImagePath(picturePath);
+			profileImg.setImageBitmap(decodeSampledBitmapFromFile(picturePath,
+					90, 90));
+
+		}
+
+	}
+
+	public static Bitmap decodeSampledBitmapFromFile(String path, int reqWidth,
+			int reqHeight) {
+
+		// First decode with inJustDecodeBounds=true to check dimensions
+		final BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inJustDecodeBounds = true;
+		BitmapFactory.decodeFile(path, options);
+
+		// Calculate inSampleSize
+		options.inSampleSize = calculateInSampleSize(options, reqWidth,
+				reqHeight);
+
+		// Decode bitmap with inSampleSize set
+		options.inJustDecodeBounds = false;
+		return BitmapFactory.decodeFile(path, options);
+	}
+
+	public static int calculateInSampleSize(BitmapFactory.Options options,
+			int reqWidth, int reqHeight) {
+		// Raw height and width of image
+		final int height = options.outHeight;
+		final int width = options.outWidth;
+		int inSampleSize = 1;
+
+		if (height > reqHeight || width > reqWidth) {
+
+			// Calculate ratios of height and width to requested height and
+			// width
+			final int heightRatio = Math.round((float) height
+					/ (float) reqHeight);
+			final int widthRatio = Math.round((float) width / (float) reqWidth);
+
+			// Choose the smallest ratio as inSampleSize value, this will
+			// guarantee
+			// a final image with both dimensions larger than or equal to the
+			// requested height and width.
+			inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+		}
+
+		return inSampleSize;
+	}
+
 }
